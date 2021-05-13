e365f5989504114f724c23c4598a556ccb48a229
Aliaksandr Bulynka <abulynka@gmail.com>
2021-05-14 00:57:51 +0300

    feat: add users support

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,33 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const users = new Map();
+
+const getAll = async () => users;
+
+const addUser = async (user) => {
+  users.set(user.id, user);
 };
 
-module.exports = { getAll };
+const getUser = async (id) => users.get(id);
+
+const updateUser = async (id, info) => {
+  if (users.has(id)) {
+    const user = users.get(id);
+    user.name = info.name;
+    user.login = info.login;
+    user.password = info.password;
+  } else {
+    throw new Error('User not found');
+  }
+  return users.get(id);
+}
+
+const deleteUser = async (id) => {
+  if (users.has(id)) {
+    const user = users.get(id);
+    users.delete(id);
+    return user;
+  } 
+    throw new Error('User not found');
+  
+}
+
+module.exports = { getAll, addUser, getUser, updateUser, deleteUser };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -3,9 +3,95 @@ const User = require('./user.model');
 const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  await usersService.getAll()
+    .then(
+      (users) => {
+        const result = [];
+        users.forEach(
+          (user) => {
+            result.push(User.toResponse(user));
+          }
+        );
+        res.json(result);
+      }
+    )
+    .catch(
+      () => {
+        res.status(500);
+        res.send();
+      }
+    );
+});
+
+router.route('/').post(async (req, res) => {
+  if (!req.body || !req.body.name || !req.body.login || !req.body.password) {
+    res.status(400);
+    res.send();
+  } else {
+    const user = new User();
+
+    user.name = req.body.name;
+    user.login = req.body.login;
+    user.password = req.body.password;
+
+    usersService.addUser(user)
+      .then(
+        (e) => {
+          res.status(201);
+          res.json(User.toResponse(e));
+        }
+      )
+      .catch(
+        () => {
+          res.status(500);
+          res.send();
+        }
+      );
+  }
+});
+
+router.route('/:id').get(async (req, res) => {
+  await usersService.getUser(req.params.id)
+    .then(
+      (user) => {
+        res.status(200);
+        res.json(User.toResponse(user));
+      }
+    ).catch(
+      () => {
+        res.status(404);
+        res.send();
+      }
+    );
+});
+
+router.route('/:id').put(async (req, res) => {
+  await usersService.updateUser(req.params.id, req.body)
+    .then(
+      (user) => {
+        res.json(User.toResponse(user));
+      }
+    ).catch(
+      () => {
+        res.status(400);
+      }
+    );
+  res.send();
+});
+
+router.route('/:id').delete(async (req, res) => {
+  await usersService.deleteUser(req.params.id)
+    .then(
+      (user) => {
+        res.status(204);
+        res.json(User.toResponse(user));
+      }
+    ).catch(
+      () => {
+        res.status(404);
+        res.send();
+      }
+    );
 });
 
 module.exports = router;
