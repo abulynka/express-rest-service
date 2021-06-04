e0d58559159cbfa7e1606d334d1847e85d6d5fde
Aliaksandr Bulynka <abulynka@gmail.com>
2021-06-04 03:19:58 +0200

    feat: make users modifications

--- src/resources/users/user.router.ts
+++ src/resources/users/user.router.ts
@@ -2,63 +2,51 @@ import express from 'express';
 import httpCodes from 'http-status-codes';
 import User from './user.model';
 import usersService from './user.service';
+import { Exception } from '../../middleware/exception';
 
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
 const getId = (params: { [key: string]: string; }): string => {
   if (params && params['id'] && typeof params['id'] === 'string') {
     return params['id'];
   }
-  throw new Error('unable to find id');
+  throw new Exception(Exception.STATUS_BAD_REQUEST, 'unable to find id');
 }
 
-router.route('/').get(async (req: express.Request, res: express.Response) => {
-  try {
-    if (!req) {
-      // just for linter and ts compiler
-    }
-    const users = await usersService.getAll();
-    const result: { [key: string]: string }[] = [];
-    users.forEach((user) => {
-      result.push(User.toResponse(user));
-    });
-    res.status(httpCodes.OK).json(result);
-  } catch (e) {
-    res.status(httpCodes.INTERNAL_SERVER_ERROR).send();
-  }
+router.route('/').get(async (_req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const users = await usersService.getAll();
+  const result: { [key: string]: string }[] = [];
+  users.forEach((user) => {
+    result.push(User.toResponse(user));
+  });
+  res.status(httpCodes.OK).json(result);
+
+  next();
 });
 
-router.route('/').post(async (req: express.Request, res: express.Response) => {
-  try {
-    res.status(httpCodes.CREATED).json(User.toResponse((await usersService.add(req.body))));
-  } catch (e) {
-    res.status(httpCodes.BAD_REQUEST).send();
-  }
+router.route('/').post(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(httpCodes.CREATED).json(User.toResponse((await usersService.add(req.body))));
+
+  next();
 });
 
-router.route('/:id').get(async (req: express.Request, res: express.Response) => {
-  try {
-    res.status(httpCodes.OK).json(User.toResponse((await usersService.get(getId(req.params)))));
-  } catch (e) {
-    res.status(httpCodes.NOT_FOUND).send();
-  }
+router.route('/:id').get(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(httpCodes.OK).json(User.toResponse((await usersService.get(getId(req.params)))));
+
+  next();
 });
 
-router.route('/:id').put(async (req: express.Request, res: express.Response) => {
-  try {
-    res.json(User.toResponse((await usersService.update(getId(req.params), req.body))));
-  } catch (e) {
-    res.status(httpCodes.BAD_REQUEST).send();
-  }
+router.route('/:id').put(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.json(User.toResponse((await usersService.update(getId(req.params), req.body))));
+
+  next();
 });
 
-router.route('/:id').delete(async (req: express.Request, res: express.Response) => {
-  try {
-    await usersService.remove(getId(req.params));
-    res.status(httpCodes.NO_CONTENT).send();
-  } catch (e) {
-    res.status(httpCodes.NOT_FOUND).send();
-  }
+router.route('/:id').delete(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  await usersService.remove(getId(req.params));
+  res.status(httpCodes.NO_CONTENT).send();
+
+  next();
 });
 
 export default router;
