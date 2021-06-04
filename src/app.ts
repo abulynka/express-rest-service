186b30010cf7e677e07c0c0b36cb87b9ceb4b9fe
Aliaksandr Bulynka <abulynka@gmail.com>
2021-06-04 03:21:56 +0200

    feat: make modifications in app

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,14 @@ import YAML from 'yamljs';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import { HandlerExpress } from './middleware/handler_express';
+import { ErrorHandler } from './middleware/error_handler';
+import { ErrorHandlerExpress } from './middleware/error_handler_express';
+
+new ErrorHandler().init();
 
 const app = express();
+
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
@@ -16,7 +22,6 @@ app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
-    return;
   }
   next();
 });
@@ -24,5 +29,14 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
+
+// app.use('/doc', (req: express.Request, _res: express.Response, next: express.NextFunction) => {
+//   swaggerDocument.host = req.get('host');
+//   Object.assign(req, {swaggerDoc: swaggerDocument});
+//   next();
+// }, swaggerUI.serve, swaggerUI.setup());
+
+app.use(new ErrorHandlerExpress().process);
+app.use('/', new HandlerExpress().process);
 
 export default app;
