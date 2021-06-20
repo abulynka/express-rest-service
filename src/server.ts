c8dd8fe7b406b2407c6dda5219c618d908b42f5b
Aliaksandr Bulynka <abulynka@gmail.com>
2021-06-20 17:13:54 +0300

    feat: change app

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import os from 'os';
 import config from './common/config';
 import app from './app';
 
 app.listen(config.PORT, () =>
-  process.stdout.write(`App is running on http://localhost:${config.PORT}`)
+  process.stdout.write(`App is running on http://localhost:${config.PORT}${os.EOL}`)
 );
